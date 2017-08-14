8b910bdb6bfa1e3a8c45edbdd3fe9d2142e9960f
Jonathan Philipos <pavilion.vps@gmail.com>
2017-08-15 03:19:26 +1000

    Update and rename hook_vertice.rb to hook_virtengine.rb

--- support/hook_virtengine.rb
+++ support/hook_virtengine.rb
@@ -20,7 +20,7 @@
 ONE_LOCATION=ENV["ONE_LOCATION"]
 
 if !ONE_LOCATION
-    CONFIG_FILE="/var/lib/megam/master_key"
+    CONFIG_FILE="/var/lib/detio/master_key"
     LOG_FILE="/var/log/one/vertice_error.log"
 else
     CONFIG_FILE=ONE_LOCATION+"/var/config"
@@ -30,6 +30,8 @@ end
 require 'nokogiri'
 require 'base64'
 require 'megam_api'
+require 'json'
+require 'rubygems'
 
 ################################################################################
 # logs
@@ -73,10 +75,10 @@ s  = Base64.decode64(content.inspect)
 KEYS = Hash[*File.read(CONFIG_FILE).split(/[= \n]+/)]
 
 
-@doc  = Nokogiri::XML(s) 
+@doc  = Nokogiri::XML(s)
 
-KEYS["status"] = STATUS
-KEYS["state"] = STATE
+					   
+					 
 KEYS[:email]   = @doc.xpath("//ACCOUNTS_ID").text
 KEYS["id"]      = @doc.xpath("//ASSEMBLY_ID").text
 KEYS["asms_id"] = @doc.xpath("//ASSEMBLIES_ID").text
@@ -84,21 +86,43 @@ KEYS[:master_key] = KEYS['masterkey']
 KEYS[:host] = KEYS['host']
 KEYS[:org_id] = @doc.xpath("//ORG_ID").text
 KEYS["org_id"] = @doc.xpath("//ORG_ID").text  #//gateway has change to get from header
- 
+
+
+
+
 ################################################################################
 # Main
 ################################################################################
 
 log "Hook - vertice #{STATE}  #{STATUS} started:"
 
+
 begin
-   m  = Megam::Assembly.update(KEYS)
+ asmb = Megam::Assembly.show(KEYS)
+asmc = asmb[:body]
+ asm = asmc.first if asmc
+ UPD_KEYS = {
+   'id' =>  asm.id,
+   'name' => asm.name,
+   "components" => asm.components,
+   "tosca_type" => asm.tosca_type,
+   "policies" => asm.policies,
+   "inputs" => asm.inputs,
+   "outputs" => asm.outputs,
+   "status" => STATUS,
+   "state" => STATE ,
+  email: KEYS[:email],
+   master_key: KEYS[:master_key],
+   host:  KEYS[:host],
+   org_id: KEYS[:org_id]
+ }
+m  = Megam::Assembly.update(UPD_KEYS)
 rescue Exception => e
-  log_error e.to_s
-  exit -1
+log_error e.to_s
+exit -1
 end
 
+												  
+
+
 log "Hook - vertice #{STATE}  #{STATUS} finished:"
-
-
-
